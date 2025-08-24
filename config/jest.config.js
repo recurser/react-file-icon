dfc66025a5d5ef975845b256a3a6eccc704e25c2
Dave Perrett <dave@recurser.com>
2025-08-25 09:54:11 +1000

    chore: fix failing tests

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['./jest.setup.js'],
   transform: {
     '\\.js$': ['babel-jest', { configFile: './config/babel.config.js' }],
